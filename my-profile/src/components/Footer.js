6d24d7b4c0317fbb3410e2d992c40eda71733cda
= <bdrgcmz@gmail.com>
2022-09-17 20:55:15 +0200

    Footer comleted

--- my-profile/src/components/Footer.js
+++ my-profile/src/components/Footer.js
@@ -2,36 +2,37 @@ import React from "react";
 
 const Footer = ({ bacgroundDark, color }) => {
   return (
-    <div className={`footer p-3 ${bacgroundDark} text-white`}>
-      <div className="social-media d-flex justify-content-around align-items-center mb-3">
-        <div className="number-email text-start">
-          <div className="my-number mb-3 d-flex align-items-center">
-            <i className="fa-solid fa-mobile-screen-button me-2 fs-5"></i>{" "}
-            <span>+46 76 963 73 90</span>
-          </div>
-          <div className="email d-flex align-items-center mb-3">
-            <i className="fa-solid fa-envelope me-2 fs-5"></i>
-            <span>bedir.gocmez@hicoders.ch</span>
-          </div>
+    <div
+      className={`footer p-3 ${bacgroundDark} text-white d-flex justify-content-around align-items-center`}
+    >
+      <div className="social-media d-flex justify-content-around align-items-center ">
+        <div className="my-number mb-2 d-flex align-items-center">
+          <i className="fa-solid fa-mobile-screen-button me-2 "></i> <span>+46 76 963 73 90</span>
         </div>
-        <div className="github-linkedin d-flex flex-column align-items-end">
-          <div className="guthub d-flex align-items-center mb-3">
-            <a className="text-white text-decoration-none" href="https://github.com/bedirgcmz">
-              https://github.com/bedirgcmz
-            </a>
-            <i className="fa-brands fa-github ms-2 fs-5"></i>
-          </div>
-          <div className="linkedin d-flex align-items-center mb-3">
-            <a
-              className="text-white text-decoration-none "
-              href="https://www.linkedin.com/in/bedir-gocmez-1a3388229/"
-            >
-              https://www.linkedin.com/in/bedir-gocmez-1a3388229/
-            </a>
-            <i className="fa-brands fa-linkedin ms-2 fs-5"></i>
-          </div>
+        <div className="email d-flex align-items-center mb-2">
+          <i className="fa-solid fa-envelope me-2 "></i>
+          <span>bedir.gocmez@hicoders.ch</span>
+        </div>
+        <div className="guthub d-flex align-items-center mb-2">
+          <a
+            className="text-white text-decoration-none social-m-link"
+            href="https://github.com/bedirgcmz"
+          >
+            <i className="fa-brands fa-github me-2 "></i>
+            Github
+          </a>
+        </div>
+        <div className="linkedin d-flex align-items-center mb-2">
+          <a
+            className="text-white text-decoration-none social-m-link"
+            href="https://www.linkedin.com/in/bedir-gocmez-1a3388229/"
+          >
+            <i className="fa-brands fa-linkedin me-2 "></i>
+            Linkedin
+          </a>
         </div>
       </div>
+      <p className="text-center copyright">&copy; Copyright 2022 Bedir Gocmez</p>
     </div>
   );
 };
